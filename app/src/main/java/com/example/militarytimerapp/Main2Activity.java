2c587feb5e970988311d516ce3bb87f5a437a1a6
김호준 <elddy0948@gmail.com>
2019-11-28 21:53:41 +0900

    -

---
+++ app/src/main/java/com/example/militarytimerapp/Main2Activity.java
@@ -0,0 +1,70 @@
+package com.example.militarytimerapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    RadioGroup military_group;
+    ImageView military_image;
+    Button date_select;
+    View date_view;
+    TextView date_text;
+    Integer y,m,d;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        military_group = (RadioGroup)findViewById(R.id.military_group);
+        military_image = (ImageView)findViewById(R.id.military_image);
+        date_select = (Button)findViewById(R.id.go_to_datepicker);
+        date_text = (TextView)findViewById(R.id.text_date);
+
+        military_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.marine){
+                    military_image.setImageResource(R.mipmap.marine_icon);
+                }
+                else if(checkedId == R.id.navy){
+                    military_image.setImageResource(R.mipmap.navy_icon);
+                }
+                else if(checkedId == R.id.airforce){
+                    military_image.setImageResource(R.mipmap.airforce_icon);
+                }
+            }
+        });
+        date_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                date_view = (View)View.inflate(Main2Activity.this,R.layout.enter_date,null);
+                AlertDialog.Builder dv = new AlertDialog.Builder(Main2Activity.this);
+                dv.setTitle("입대 날짜 선택");
+                dv.setView(date_view);
+                dv.setPositiveButton("선택", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatePicker datePicker = (DatePicker)date_view.findViewById(R.id.date_picker);
+                        //18개월
+                        y = datePicker.getYear();
+                        m = datePicker.getMonth() + 1;
+                        d = datePicker.getDayOfMonth();
+                        date_text.setText("입대일 : "+y.toString()+"년 "+m.toString()+"월 "+d.toString()+"일");
+                    }
+                });
+                dv.setNegativeButton("닫기",null);
+                dv.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/militarytimerapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.militarytimerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start_button = (Button)findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
